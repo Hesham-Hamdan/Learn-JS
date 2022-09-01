11169fc14f1620fad9a643cbae5931833c1693dd
Hesham Hamdan <>
2022-09-01 22:41:55 +0200

    Third Week

---
+++ 18-22/Assignment 2/main.js
@@ -0,0 +1,21 @@
+let num = 3;
+
+// Solution One
+console.log(num + num); //6
+
+// Solution Two
+console.log(num * num - num); //6
+
+// Soultion Three
+console.log(true + true + true + num); //6
+
+// Soultion Four
+console.log(( num - true ) * num); //6
+
+// Solution Five
+console.log(num ** (--num) - ++num); //6
+
+// Solution Six
+console.log(num * --num); //6
+
+
