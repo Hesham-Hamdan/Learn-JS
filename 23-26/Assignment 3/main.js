35d09f2db525de9b2091c2ae5c1238507ac6b6e9
Hesham Hamdan <112446931+Hesham-Hamdan@users.noreply.github.com>
2022-09-02 13:31:33 +0200

    Delete 23-26 directory

--- 23-26/Assignment 3/main.js
+++
@@ -1,6 +0,0 @@
-console.log(('MAX_SAFE_INTEGER').length); // 16
-
-
-// Another Solution
-
-console.log(Math.ceil(Math.log10(Number.MAX_SAFE_INTEGER)))
--- 23-26/Assignment 7/main.js
+++
@@ -1,1 +0,0 @@
-console.log(4 * (Math.random())); // 0 || 1 || 2 || 3 || 4
