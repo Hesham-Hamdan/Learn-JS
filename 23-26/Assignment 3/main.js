eea0565fd33e5ab261b6a4807a458b65c1c76ef1
Hesham Hamdan <heshamhamdan2018@gmail.com>
2022-09-02 13:33:27 +0200

    Fourth Week Part 1

---
+++ 23-26/Assignment 3/main.js
@@ -0,0 +1,6 @@
+console.log(('MAX_SAFE_INTEGER').length); // 16
+
+
+// Another Solution
+
+console.log(Math.ceil(Math.log10(Number.MAX_SAFE_INTEGER)))
---
+++ 23-26/Assignment 7/main.js
@@ -0,0 +1,1 @@
+console.log(4 * (Math.random())); // 0 || 1 || 2 || 3 || 4
