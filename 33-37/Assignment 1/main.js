b9b97530392df05ec0a797606f4c9ca6322979d6
Hesham Hamdan <heshamhamdan2018@gmail.com>
2022-09-03 01:14:21 +0200

    Fifth Week Part 2

---
+++ 33-37/Assignment 1/main.js
@@ -0,0 +1,20 @@
+// Test Case 1
+let num = 9; // "009"
+
+// Test Case 2
+let num = 20; // "020"
+
+// Test Case 3
+let num = 110; // "110"
+
+
+
+if(num < 10) {  
+    console.log(num = "00" + num); 
+} else if (num > 10 && num < 100) {
+    console.log(num = '0' + num);
+} else if (num >= 100 ) {
+    console.log(num);
+}
+
+
